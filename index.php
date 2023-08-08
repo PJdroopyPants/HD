d8dd4de0fcb35d86f34e2a62dda8b4bb11b645e8
PJdroopyPants <60212379+PJdroopyPants@users.noreply.github.com>
2023-08-08 04:37:33 -0400

    Update index.php

--- index.php
+++ index.php
@@ -297,14 +297,14 @@ function myCoolFunction() {
 
 	// set up scrollTrigger animation for the when the intro scrolls out
 
-	let stl = gsap.timeline({
-		scrollTrigger: {
-			trigger: ".intro",
-			scrub: 1,
-			start: "top bottom", // position of trigger meets the scroller position
-			end: "bottom top"
-		}
-	});
+//	let stl = gsap.timeline({
+//		scrollTrigger: {
+//			trigger: ".intro",
+//			scrub: 1,
+//			start: "top bottom", // position of trigger meets the scroller position
+//			end: "bottom top"
+//		}
+//	});
 
 	stl.to(".intro__title", {
 		x: 400,
@@ -321,246 +321,246 @@ function myCoolFunction() {
 	);
 }
 
-function initLinks() {
-	// ScrollToPlugin links
+//function initLinks() {
+//	// ScrollToPlugin links
+//
+//	links.forEach((link, index, e) => {
+//		let linkST = link.querySelector(".slide__scroll-line");
+//
+//		link.addEventListener("click", (e) => {
+//			e.preventDefault();
+//			gsap.to(window, {
+//				duration: 2,
+//				scrollTo: {
+//					y: "#slide-" + (index + 3)
+//				},
+//				ease: "power2.inOut"
+//			});
+//			slideID++;
+//		});
+//
+//		link.addEventListener("mouseover", (e) => {
+//			gsap.to(linkST, {
+//				y: 40,
+//				transformOrigin: "bottom center",
+//				duration: 0.6,
+//				ease: "power4"
+//			});
+//		});
+//
+//		link.addEventListener("mouseout", (e) => {
+//			gsap.to(linkST, {
+//				y: 0,
+//				transformOrigin: "bottom center",
+//				duration: 0.6,
+//				ease: "power4"
+//			});
+//		});
+//	});
+//
+//	// ScrollToPlugin link back to the top
+//
+//	let top = select(".footer__link-top");
+//
+//	top.addEventListener("click", (e) => {
+//		e.preventDefault();
+//		scrollTop();
+//	});
+//
+//	top.addEventListener("mouseover", (e) => {
+//		gsap.to(".footer__link-top-line", {
+//			scaleY: 3,
+//			transformOrigin: "bottom center",
+//			duration: 0.6,
+//			ease: "power4"
+//		});
+//	});
+//
+//	top.addEventListener("mouseout", (e) => {
+//		gsap.to(".footer__link-top-line", {
+//			scaleY: 1,
+//			transformOrigin: "bottom center",
+//			duration: 0.6,
+//			ease: "power4"
+//		});
+//	});
+//
+//	// Dummy slide links
+//
+//	let slideLinks = selectAll(".slide-link");
+//
+//	slideLinks.forEach((slideLink, index, e) => {
+//		let slideL = slideLink.querySelector(".slide-link__line");
+//
+//		slideLink.addEventListener("mouseover", (e) => {
+//			gsap.to(slideL, {
+//				x: 20,
+//				scaleX: 0.3,
+//				transformOrigin: "right center",
+//				duration: 0.8,
+//				ease: "power4"
+//			});
+//		});
+//		slideLink.addEventListener("mouseout", (e) => {
+//			gsap.to(slideL, {
+//				x: 0,
+//				scaleX: 1,
+//				transformOrigin: "right center",
+//				duration: 0.8,
+//				ease: "power4"
+//			});
+//		});
+//	});
+//}
 
-	links.forEach((link, index, e) => {
-		let linkST = link.querySelector(".slide__scroll-line");
+//function initSlides() {
+//	// Animation of each slide scrolling into view
+//
+//	slides.forEach((slide, i) => {
+//		let tl = gsap.timeline({
+//			scrollTrigger: {
+//				trigger: slide,
+//				start: "40% 50%" // position of trigger meets the scroller position
+//			}
+//		});
+//
+//		tl.from(slide.querySelectorAll(".col__content-title"), {
+//			ease: "power4",
+//			y: "+=5vh",
+//			duration: 2.5
+//		})
+//			.from(
+//				slide.querySelectorAll(".line__inner"),
+//				{
+//					x: -800,
+//					duration: 2,
+//					ease: "power4",
+//					stagger: 0.1
+//				},
+//				0
+//			)
+//			.from(
+//				slide.querySelectorAll(".col__content-txt"),
+//				{
+//					x: 100,
+//					y: 50,
+//					opacity: 0,
+//					duration: 2,
+//					ease: "power4"
+//				},
+//				0.4
+//			)
+//			.from(
+//				slide.querySelectorAll(".slide-link"),
+//				{
+//					x: -100,
+//					y: 100,
+//					opacity: 0,
+//					duration: 2,
+//					ease: "power4"
+//				},
+//				0.3
+//			)
+//			.from(
+//				slide.querySelectorAll(".slide__scroll-link"),
+//				{
+//					y: 200,
+//					duration: 3,
+//					ease: "power4"
+//				},
+//				0.4
+//			)
+//			.to(
+//				slide.querySelectorAll(".slide__scroll-line"),
+//				{
+//					scaleY: 0.6,
+//					transformOrigin: "bottom left",
+//					duration: 2.5,
+//					ease: "elastic(1,0.5)"
+//				},
+//				1.4
+//			);
+//	});
+//
+//	// External footer link scroll animation
+//
+//	gsap.from(".footer__link", {
+//		scrollTrigger: {
+//			trigger: ".footer",
+//			scrub: 2,
+//			start: "50% 100%", // position of trigger meets the scroller position
+//			end: "0% 0%"
+//		},
+//		y: "20vh",
+//		ease: "sine"
+//	});
+//}
 
-		link.addEventListener("click", (e) => {
-			e.preventDefault();
-			gsap.to(window, {
-				duration: 2,
-				scrollTo: {
-					y: "#slide-" + (index + 3)
-				},
-				ease: "power2.inOut"
-			});
-			slideID++;
-		});
+//function initParallax() {
+//	slides.forEach((slide, i) => {
+//		let imageWrappers = slide.querySelectorAll(".col__image-wrap");
+//
+//		gsap.fromTo(
+//			imageWrappers,
+//			{
+//				y: "-30vh"
+//			},
+//			{
+//				y: "30vh",
+//				scrollTrigger: {
+//					trigger: slide,
+//					scrub: true,
+//					start: "top bottom", // position of trigger meets the scroller position
+//					snap: {
+//						snapTo: 0.5, // 0.5 'cause the scroll animation range is 200vh for parallax effect
+//						duration: 1,
+//						ease: "power4.inOut"
+//					}
+//				},
+//				ease: "none"
+//			}
+//		);
+//	});
+//}
 
-		link.addEventListener("mouseover", (e) => {
-			gsap.to(linkST, {
-				y: 40,
-				transformOrigin: "bottom center",
-				duration: 0.6,
-				ease: "power4"
-			});
-		});
+//function scrollTop() {
+//	gsap.to(window, {
+//		duration: 2,
+//		scrollTo: {
+//			y: "#slide-0"
+//		},
+//		ease: "power2.inOut"
+//	});
+//	gsap.to(".footer__link-top-line", {
+//		scaleY: 1,
+//		transformOrigin: "bottom center",
+//		duration: 0.6,
+//		ease: "power4"
+//	});
+//}
 
-		link.addEventListener("mouseout", (e) => {
-			gsap.to(linkST, {
-				y: 0,
-				transformOrigin: "bottom center",
-				duration: 0.6,
-				ease: "power4"
-			});
-		});
-	});
-
-	// ScrollToPlugin link back to the top
-
-	let top = select(".footer__link-top");
-
-	top.addEventListener("click", (e) => {
-		e.preventDefault();
-		scrollTop();
-	});
-
-	top.addEventListener("mouseover", (e) => {
-		gsap.to(".footer__link-top-line", {
-			scaleY: 3,
-			transformOrigin: "bottom center",
-			duration: 0.6,
-			ease: "power4"
-		});
-	});
-
-	top.addEventListener("mouseout", (e) => {
-		gsap.to(".footer__link-top-line", {
-			scaleY: 1,
-			transformOrigin: "bottom center",
-			duration: 0.6,
-			ease: "power4"
-		});
-	});
-
-	// Dummy slide links
-
-	let slideLinks = selectAll(".slide-link");
-
-	slideLinks.forEach((slideLink, index, e) => {
-		let slideL = slideLink.querySelector(".slide-link__line");
-
-		slideLink.addEventListener("mouseover", (e) => {
-			gsap.to(slideL, {
-				x: 20,
-				scaleX: 0.3,
-				transformOrigin: "right center",
-				duration: 0.8,
-				ease: "power4"
-			});
-		});
-		slideLink.addEventListener("mouseout", (e) => {
-			gsap.to(slideL, {
-				x: 0,
-				scaleX: 1,
-				transformOrigin: "right center",
-				duration: 0.8,
-				ease: "power4"
-			});
-		});
-	});
-}
-
-function initSlides() {
-	// Animation of each slide scrolling into view
-
-	slides.forEach((slide, i) => {
-		let tl = gsap.timeline({
-			scrollTrigger: {
-				trigger: slide,
-				start: "40% 50%" // position of trigger meets the scroller position
-			}
-		});
-
-		tl.from(slide.querySelectorAll(".col__content-title"), {
-			ease: "power4",
-			y: "+=5vh",
-			duration: 2.5
-		})
-			.from(
-				slide.querySelectorAll(".line__inner"),
-				{
-					x: -800,
-					duration: 2,
-					ease: "power4",
-					stagger: 0.1
-				},
-				0
-			)
-			.from(
-				slide.querySelectorAll(".col__content-txt"),
-				{
-					x: 100,
-					y: 50,
-					opacity: 0,
-					duration: 2,
-					ease: "power4"
-				},
-				0.4
-			)
-			.from(
-				slide.querySelectorAll(".slide-link"),
-				{
-					x: -100,
-					y: 100,
-					opacity: 0,
-					duration: 2,
-					ease: "power4"
-				},
-				0.3
-			)
-			.from(
-				slide.querySelectorAll(".slide__scroll-link"),
-				{
-					y: 200,
-					duration: 3,
-					ease: "power4"
-				},
-				0.4
-			)
-			.to(
-				slide.querySelectorAll(".slide__scroll-line"),
-				{
-					scaleY: 0.6,
-					transformOrigin: "bottom left",
-					duration: 2.5,
-					ease: "elastic(1,0.5)"
-				},
-				1.4
-			);
-	});
-
-	// External footer link scroll animation
-
-	gsap.from(".footer__link", {
-		scrollTrigger: {
-			trigger: ".footer",
-			scrub: 2,
-			start: "50% 100%", // position of trigger meets the scroller position
-			end: "0% 0%"
-		},
-		y: "20vh",
-		ease: "sine"
-	});
-}
-
-function initParallax() {
-	slides.forEach((slide, i) => {
-		let imageWrappers = slide.querySelectorAll(".col__image-wrap");
-
-		gsap.fromTo(
-			imageWrappers,
-			{
-				y: "-30vh"
-			},
-			{
-				y: "30vh",
-				scrollTrigger: {
-					trigger: slide,
-					scrub: true,
-					start: "top bottom", // position of trigger meets the scroller position
-					snap: {
-						snapTo: 0.5, // 0.5 'cause the scroll animation range is 200vh for parallax effect
-						duration: 1,
-						ease: "power4.inOut"
-					}
-				},
-				ease: "none"
-			}
-		);
-	});
-}
-
-function scrollTop() {
-	gsap.to(window, {
-		duration: 2,
-		scrollTo: {
-			y: "#slide-0"
-		},
-		ease: "power2.inOut"
-	});
-	gsap.to(".footer__link-top-line", {
-		scaleY: 1,
-		transformOrigin: "bottom center",
-		duration: 0.6,
-		ease: "power4"
-	});
-}
-
-function initKeys() {
-	document.addEventListener("keydown", (e) => {
-		e.preventDefault();
-		if (event.keyCode == 40) {
-			//down arrow to next slide
-			if (slideID <= slides.length) {
-				slideID++;
-				gsap.to(window, {
-					duration: 2,
-					scrollTo: {
-						y: "#slide-" + slideID
-					},
-					ease: "power2.inOut"
-				});
-			}
-		} else if (event.keyCode == 38) {
-			// up arrow to top
-			slideID = 0;
-			scrollTop();
-		}
-	});
-}
+//function initKeys() {
+//	document.addEventListener("keydown", (e) => {
+//		e.preventDefault();
+//		if (event.keyCode == 40) {
+//			//down arrow to next slide
+//			if (slideID <= slides.length) {
+//				slideID++;
+//				gsap.to(window, {
+//					duration: 2,
+//					scrollTo: {
+//						y: "#slide-" + slideID
+//					},
+//					ease: "power2.inOut"
+//				});
+//			}
+//		} else if (event.keyCode == 38) {
+//			// up arrow to top
+//			slideID = 0;
+//			scrollTop();
+//		}
+//	});
+//}
 
 function init() {
 	gsap.set(stage, { autoAlpha: 1 });
